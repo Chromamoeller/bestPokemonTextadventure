eedaa69c6058ae89434bc2f7b3d31c6d56cdfac4
Chrism <der.chris.moeller@gmail.com>
2025-06-03 22:50:12 +0200

    Delete That Bullshit

--- VueTextadventurePokemon/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>
-    <h1 class="text-3xl font-bold text-blue-500 underline">Hello world!</h1>
-  </div>
-</template>
-
-<style scoped></style>
